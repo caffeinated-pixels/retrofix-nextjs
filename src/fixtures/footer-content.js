8f8a9e46dd44e82408351467b1ffab8b419c01f0
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2024-04-06 12:03:28 -0400

    add assets

---
+++ src/fixtures/footer-content.js
@@ -0,0 +1,39 @@
+export const footerHomeContent = {
+  title: "Questions? Don't Call 1-555-123-4567",
+  body: [
+    'FAQ',
+    'Help Center',
+    'Account',
+    'Media Center',
+
+    'Investor Relations',
+    'Jobs',
+    'Redeem Gift Cards',
+    'Buy Gift Cards',
+
+    'Ways to Watch',
+    'Terms of Use',
+    'Privacy',
+    'Cookie Preferences',
+
+    'Corporate Information',
+    'Contact Us',
+    'Speed Test',
+    'Legal Notices',
+
+    'Only on RetroFix',
+  ],
+  bottomText: 'RetroFix Canada',
+}
+
+export const footerHomeRegistration = {
+  title: "Questions? Don't Call 1-555-123-4567",
+  body: [
+    'FAQ',
+    'Help Center',
+    'Terms of Use',
+    'Privacy',
+    'Cookie Preferences',
+    'Corporate Information',
+  ],
+}
