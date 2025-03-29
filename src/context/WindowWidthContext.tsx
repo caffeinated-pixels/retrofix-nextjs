800ffde955e0b3563cf4971984f338e45a867b96
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2025-03-29 08:33:55 -0400

    fix useLayoutEffect warning on server

--- src/context/WindowWidthContext.tsx
+++ src/context/WindowWidthContext.tsx
@@ -9,10 +9,16 @@ import {
 const WindowWidthContext = createContext<number | null>(null)
 
 export const WindowWidthContextProvider = ({ children }: PropsWithChildren) => {
-  const [width, setWidth] = useState(window.innerWidth)
+  const [width, setWidth] = useState(0)
 
   // useLayoutEffect runs before React renders the component making it a better choice for DOM measurement than useEffect, which runs after rendering/screen painting
   useLayoutEffect(() => {
+    if (typeof window === 'undefined') return
+
+    if (width === 0) {
+      setWidth(window.innerWidth)
+    }
+
     const updateWidth = () => setWidth(window.innerWidth)
 
     // updates state when screen width changes
