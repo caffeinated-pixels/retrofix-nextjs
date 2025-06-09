419c4f0e1b4c4c8ad50adcf707080949d219ac1a
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2025-06-09 08:13:38 -0400

    refactor: enhance BrowsePage layout with footer and container

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -2,11 +2,19 @@
 
 import { BrowseDesktopLayout } from '@/components/browse/BrowseDesktopLayout'
 import { BrowseMobileLayout } from '@/components/browse/BrowseMobileLayout'
+import { BrowsePageContainer } from '@/components/shared/containers/BrowsePageContainer'
+import { Footer } from '@/components/shared/footer'
+import { footerHomeContent } from '@/fixtures/footer-content'
 import { useWindowWidth } from '@/hooks/useWindowWidth'
 
 export default function BrowsePage() {
   const width = useWindowWidth()
   const isMobile = width ? width < 768 : false
 
-  return isMobile ? <BrowseMobileLayout /> : <BrowseDesktopLayout />
+  return (
+    <BrowsePageContainer>
+      {isMobile ? <BrowseMobileLayout /> : <BrowseDesktopLayout />}
+      <Footer footerContent={footerHomeContent} increasedPadding />
+    </BrowsePageContainer>
+  )
 }
--- src/components/browse/BrowseDesktopLayout.tsx
+++ src/components/browse/BrowseDesktopLayout.tsx
@@ -1,15 +1,11 @@
 import { BrowseHeaderDesktop } from '@/components/browse/Header/BrowseHeaderDesktop'
 import { StreamingContentDesktop } from '@/components/browse/streaming-content/StreamingContentDesktop'
-import { BrowsePageContainer } from '@/components/shared/containers/BrowsePageContainer'
-import { Footer } from '@/components/shared/footer'
-import { footerHomeContent } from '@/fixtures/footer-content'
 
 export const BrowseDesktopLayout = () => {
   return (
-    <BrowsePageContainer>
+    <>
       <BrowseHeaderDesktop />
       <StreamingContentDesktop />
-      <Footer footerContent={footerHomeContent} increasedPadding />
-    </BrowsePageContainer>
+    </>
   )
 }
