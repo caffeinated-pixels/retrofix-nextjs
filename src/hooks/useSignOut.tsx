79c0399cb8448f8cf82812701f16fa0b12d14f78
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2025-06-23 09:14:35 -0400

    fix: not always redirecting after logout (#1)

--- src/hooks/useSignOut.tsx
+++ src/hooks/useSignOut.tsx
@@ -1,18 +1,15 @@
 import { signOut } from 'firebase/auth'
-import { useRouter } from 'next/navigation'
 
 import { HOME } from '@/constants/routes'
 import { firebaseAuthWeb } from '@/lib/firebase/firebaseClient'
 
 export const useSignOut = () => {
-  const router = useRouter()
-
   const handleSignOut = async () => {
     await signOut(firebaseAuthWeb)
 
     await fetch('/api/logout')
 
-    router.replace(HOME)
+    window.location.replace(HOME)
   }
 
   return handleSignOut
